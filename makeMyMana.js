04a1494a3cd08af723e78b1a42ba26265fa38744
Will Luft <william.d.luft@gmail.com>
2018-05-06 20:38:37 -0400

    Add event listeners to receive inputs from form

--- makeMyMana.js
+++ makeMyMana.js
@@ -1,40 +1,45 @@
-// HOW MANY LANDS DO YOU NEED
+// INITIALIZE VARIABLES
 var landsNeeded = 23;
+var colorPips   = [];
+var totalPips   = 0;
+var numLands    = [];
 
-// HOW MANY OF EACH COLOR PIPS DO YOU HAVE
-// ARRAY IS IN WUBRG ORDER
-var colorPips = [0, 0, 27, 0, 14];
+$(".btn").click(function(event){
 
-// TOTAL PIPS
-// var totalPips = whitePips + bluePips + blackPips + redPips + greenPips;
-var totalPips = 0;
+	// GET LAND COUNT AND COLOR PIP COUNT
+	landsNeeded = Number($("#landCount").val());
+	colorPips.push(Number($("#whitePips").val()));
+	colorPips.push(Number($("#bluePips").val()));
+	colorPips.push(Number($("#blackPips").val()));
+	colorPips.push(Number($("#redPips").val()));
+	colorPips.push(Number($("#greenPips").val()));
+	console.log("lands needed: " + landsNeeded);
+	console.log("how many of each color");
+	console.log(colorPips);
 
-getTotalPips(colorPips);
+	// TOTAL PIPS
+	getTotalPips(colorPips);
+	console.log("total pips: " + totalPips);
 
+	// RATIO FOR EACH COLOR
+	landRatio(landsNeeded);
+
+	event.preventDefault();
+});
+
+
+// ===================
+// FUNCTIONS
 function getTotalPips(arr){
 	for(var i = 0; i < arr.length; i++){
 		totalPips += arr[i];
-	}
-}
+	};
+};
 
-// RATIO FOR EACH COLOR
-var numLands = [];
-
-landRatio();
-
-function landRatio(){
+function landRatio(totalLands){
+	console.log("landRatio function is running");
 	for(var i = 0; i < colorPips.length; i++){
-		numLands.push(Math.round((colorPips[i] / totalPips) * landsNeeded));
-		$(".colorsNeeded").append("<li>" + numLands[i] + "</li>")
-	}
-}
-
-var howManyLands = new Object();
-
-howManyLands.plains    = numLands[0];
-howManyLands.islands   = numLands[1];
-howManyLands.swamps    = numLands[2];
-howManyLands.mountains = numLands[3];
-howManyLands.forests   = numLands[4];
-
-console.log(howManyLands);
+		numLands.push(Math.round((colorPips[i] / totalPips) * totalLands));
+		$(".colorsNeeded").append("<li>" + numLands[i] + "</li>");
+	};
+};
